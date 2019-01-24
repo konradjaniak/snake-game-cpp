6e589f3814ccc15b3e2d84b15cc6bef7a7428da3
Konrad <konrad.janiak@gmail.com>
2019-01-24 22:32:39 +0100

    Add steering and score

--- Project_Snake.cpp
+++ Project_Snake.cpp
@@ -2,6 +2,8 @@
 #include <iostream>
 #include <ctime>
 #include <cstdlib>
+#include <Windows.h>
+#include <conio.h>
 
 bool bGameOver;
 const int WIDTH = 20;
@@ -25,13 +27,13 @@ int main()
 		Draw();
 		Input();
 		Logic();
-		//Sleep(10);
+		Sleep(100);
 	}
 
 	return 0;
 }
 
-/* ----------------------------- */
+/* ----------------------------- */ 
 void Setup()
 {
 	bGameOver = false;
@@ -47,19 +49,26 @@ void Setup()
 void Draw()
 {
 	system("cls");
-	/* Print top row */
+
+	// print top row
 	for(int i = 0; i < WIDTH + 1; i++)
 	{
 		std::cout << "#";
 	}
 	std::cout << std::endl;
-	/* Print side walls */
+
+	// print side walls
 	for(int i = 0; i < HEIGHT; i++)
 	{
 		for(int j = 0; j < WIDTH; j++)
 		{
 			if(j == 0)
 				std::cout << "#";
+
+			if(i == y && j == x)
+				std::cout << "0";
+			else if(i == fruitY && j == fruitX)
+				std::cout << "F";
 			else
 				std::cout << " ";
 			if(j == WIDTH - 1)
@@ -67,22 +76,77 @@ void Draw()
 		}
 		std::cout << std::endl;
 	}
-	/* Print bottom row */
+
+	// print bottom row
 	for(int i = 0; i < WIDTH + 1; i++)
 	{
 		std::cout << "#";
 	}
 	std::cout << std::endl;
+
+	// print score
+	std::cout << "Score: " << score << std::endl;
 }
 
 /* ----------------------------- */
 void Input()
 {
-
+	if(_kbhit())
+	{
+		switch(_getch())
+		{
+		case 'a':
+			dir = LEFT;
+			break;
+		case 'd':
+			dir = RIGHT;
+			break;
+		case 'w':
+			dir = UP;
+			break;
+		case 's':
+			dir = DOWN;
+			break;
+		case 'x':
+			bGameOver = true;
+			break;
+		}
+	}
 }
 
 /* ----------------------------- */
 void Logic()
 {
+	// change movement direction
+	switch(dir)
+	{
+	case LEFT:
+		x--;
+		break;
+	case RIGHT:
+		x++;
+		break;
+	case UP:
+		y--;
+		break;
+	case DOWN:
+		y++;
+		break;
+	default:
+		break;
+	}
 
+	// finish game when snake hit the wall
+	if(x > WIDTH || x < 0 || y > HEIGHT || y < 0)
+	{
+		bGameOver = true;
+	}
+
+	// increase the score when snake eats the fruit and set new location of the fruit
+	if(x == fruitX && y == fruitY)
+	{
+		score += 10;
+		fruitX = rand() % WIDTH;
+		fruitY = rand() % HEIGHT;
+	}
 }
